3265e0a30dc1fd9780720e4c8ea0b1c1c86f23c0
SaraAhl <sara-ahl@live.se>
2026-04-08 21:45:54 +0200

    Add IAnimal interface and implement fetchAnimals

--- src/modules/fetchAnimals.ts
+++ src/modules/fetchAnimals.ts
@@ -1,5 +1,15 @@
+import { IAnimal } from "./IAnimal";
 //Använd fetch för att hämta datan i data/data.json
 //Använd try/catch och om anropet lyckas, returnera datan. Annars, returnera "null"
 //Typa upp funktionen med vad den returnerar
 
-export default async function fetchAnimals() {}
+export default async function fetchAnimals(): Promise<IAnimal[] | null> {
+    try {
+        const response = await fetch("data/data.json");
+        const data: IAnimal[] = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching animals:", error);
+        return null;
+    }
+}
